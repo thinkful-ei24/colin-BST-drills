28091fd7dbb9a1707d16246b92b6e4721b940716
Colin Rupp <colin@thinkful.com>
2018-11-13 15:02:52 -0800

    struggled over third biggest

--- BST-drills.js
+++ BST-drills.js
@@ -14,23 +14,46 @@ function bstHeight(tree) {
   //
 }
 
+function isBst(node, min=Number.NEGATIVE_INFINITY, max=Infinity) {
+  if (node === null) {
+    return true;
+  }
+
+  if (node.key < min || node.key > max) {
+    return false;
+  }
+  return isBst(node.left, min, node.key) && isBst(node.right, node.key, max);
+}
+
+function thirdLargest(node) {
+  //find the biggest value
+  //check if biggest has a left value
+  //  if it does, thats the second biggest value
+  //    making biggest.parent the third largest
+  //  if it doesn't, then the parent is the second biggest
+  //    making biggest.parent.parent the third largest
+}
+
 
 
 function main() {
   let tree = new BST();
 
-  tree.insert(8, 'cat');
-  tree.insert(4, 'dog');
-  tree.insert(12, 'frog');
-  tree.insert(1, 'toad');
-  tree.insert(6, 'leopard');
-  tree.insert(7, 'giraffe');
-  tree.insert(20, 'hippo');
-  tree.insert(15, 'hippo');
+  tree.insert(8, 'some string');
+  tree.insert(4, 'some other string');
+  tree.insert(12, [1, 2, 3, 4, 5, 6, 5]);
+  tree.insert(1, 1);
+  tree.insert(6, 6);
+  tree.insert(7, 7);
+  tree.insert(20, 20);
+  tree.insert(10, 10);
   //tree.remove(4);
-  console.log(tree);
+  //console.log(tree);
   console.log(bstHeight(tree));
-
+  console.log(isBst(tree));
+  console.log(tree.find(12));
+  console.log('NOISE !!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  console.log(inorder(tree));
 }
 
 console.log(main());
